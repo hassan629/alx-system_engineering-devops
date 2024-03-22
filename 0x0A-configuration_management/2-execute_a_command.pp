cc95caf0cebd5018caf0b7f47e583dbda13a5628
hassan629 <hsnh83099.com>
2024-03-23 01:10:06 +0200

    done

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
-# Kills a process named killmenow
-
-exec { 'killmenow':
-  command  => 'pkill killmenow',
+# https://puppet.com/docs/puppet/3.8/type.html#exec
+# Kills a process
+exec {  'killmenow':
+  command  => '/usr/bin/pkill killmenow',
   provider => 'shell',
+  returns  => [0, 1],
 }
