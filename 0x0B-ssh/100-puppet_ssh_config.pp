ae8d2a29015a90a129db75909d6550c6586eac30
hassan629 <hsnh83099.com>
2024-03-24 11:58:25 +0200

    done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,16 @@
-# enables connection to server without a password
+# Seting up my client config file
+include stdlib
 
-file_line {
-  'passAuth':
+file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '   PasswordAuthentication no'
-  ;
-  'keyLocation':
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '   IdentityFile ~/.ssh/holberton'
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
